2e67123eaf80ff6908ff942ab467b1f0d1410a22
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-04-19 12:45:08 -0500

    updating the file bash
    updating file bash

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "\n" >> /home/vagrant/.bashrc
-echo 'export PS1="\[\e[01;34m\]microserver\[\e[0m\]\[\e[01;37m\]:\w\[\e[0m\]\[\e[00;37m\]\n\\$ \[\e[0m\]"' >> /home/vagrant/.bashrc
+echo 'export PS1="\[\e[01;34m\]sekc\[\e[0m\]\[\e[01;37m\]:\w\[\e[0m\]\[\e[00;37m\]\n\\$ \[\e[0m\]"' >> /home/vagrant/.bashrc
 printf "\n" >> /home/vagrant/.bashrc
 
 sudo yum clean all
