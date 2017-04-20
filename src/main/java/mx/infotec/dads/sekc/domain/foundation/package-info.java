32b78bec08b8a27e5853d544251dfeaa939f1ed7
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2017-04-19 20:21:57 -0500

    adding package-info.java
    adding package-info.java

---
+++ src/main/java/mx/infotec/dads/sekc/domain/foundation/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * ActivitySpaceAndActivity, contains elements to enrich practices by expressing
+ * activities.
+ * 
+ * <p>
+ * The intention of the ActivitySpaceAndActivity package is to provide
+ * additional elements to deal with more advanced practices. The elements and
+ * their relationships are presented in the diagrams shown below. A detailed
+ * definition of each of the elements is found below.
+ * 
+ * @since 1.1
+ */
+package mx.infotec.dads.sekc.domain.foundation;
