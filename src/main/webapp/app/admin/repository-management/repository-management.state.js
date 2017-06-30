6ea12abd78bc639d9b16d5e3c7b130c3ab096daa
Unknown <are.rodriguez.torres@gmail.com>
2017-06-30 13:33:15 -0500

    Add repository

---
+++ src/main/webapp/app/admin/repository-management/repository-management.state.js
@@ -0,0 +1,53 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('sekcApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider
+        .state('repository-management-create', {
+            parent: 'repository-management',
+            url: '/repository-management?page&sort',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'repository.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/admin/repository-management/repository-management-create.html',
+                    controller: '',
+                    controllerAs: 'vm'
+                }
+            },            params: {
+                page: {
+                    value: '1',
+                    squash: true
+                },
+                sort: {
+                    value: 'id,asc',
+                    squash: true
+                }
+            },
+            resolve: {
+                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                    return {
+                        page: PaginationUtil.parsePage($stateParams.page),
+                        sort: $stateParams.sort,
+                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                        ascending: PaginationUtil.parseAscending($stateParams.sort)
+                    };
+                }],
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('repository');
+                    return $translate.refresh();
+                }]
+
+            }        });
+
+
+    }
+})();
