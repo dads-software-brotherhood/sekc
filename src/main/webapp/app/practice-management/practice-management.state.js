dc6d2c9e10d44dd61162fb11701b0582e9bb33ca
Unknown <are.rodriguez.torres@gmail.com>
2017-07-26 13:56:55 -0500

    Competency

--- src/main/webapp/app/practice-management/practice-management.state.js
+++ src/main/webapp/app/practice-management/practice-management.state.js
@@ -333,6 +333,27 @@
                 }]
             }
         })
+           .state('practice-consult-competency', {
+            parent: 'practice-management',
+            url: '/practiceConsultCompetency/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-consult-competency.html',
+                    //controller: 'PracticeManagementThingsWorkController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
         
          .state('practice-management-completition', {
             parent: 'practice-management',
