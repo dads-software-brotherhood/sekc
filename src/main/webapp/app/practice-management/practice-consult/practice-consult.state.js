aec8a1b77d420bde4f51d237c25e2099ba13118b
Unknown <are.rodriguez.torres@gmail.com>
2017-08-02 14:05:05 -0500

    Practice-consult carpeta

---
+++ src/main/webapp/app/practice-management/practice-consult/practice-consult.state.js
@@ -0,0 +1,380 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('sekcApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider
+        .state('practice-management', {
+            parent: 'app',
+            url: '/practice-management?page&sort',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practiceManagement.home.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-management.html',
+                    controller: 'PracticeManagementController',
+                    controllerAs: 'vm'
+                }
+            },            params: {
+                page: {
+                    value: '1',
+                    squash: true
+                },
+                sort: {
+                    value: 'id,asc',
+                    squash: true
+                }
+            },
+            resolve: {
+                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                    return {
+                        page: PaginationUtil.parsePage($stateParams.page),
+                        sort: $stateParams.sort,
+                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                        ascending: PaginationUtil.parseAscending($stateParams.sort)
+                    };
+                }],
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+
+            }        })
+        .state('practice-management.new', {
+            url: '/new',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/practice-management/practice-management-dialog.html',
+                    controller: 'PracticeManagementDialogController',
+                    controllerAs: 'vm',
+                    backdrop: 'static',
+                    size: 'lg',
+                    resolve: {
+                        entity: function () {
+                            return {
+                                id: null, login: null, firstName: null, lastName: null, email: null,
+                                activated: true, langKey: null, createdBy: null, createdDate: null,
+                                lastModifiedBy: null, lastModifiedDate: null, resetDate: null,
+                                resetKey: null, authorities: null
+                            };
+                        }
+                    }
+                }).result.then(function() {
+                    $state.go('practice-management', null, { reload: true });
+                }, function() {
+                    $state.go('practice-management');
+                });
+            }]
+        })
+        .state('practice-management.edit', {
+            url: '/{login}/edit',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/practice-management/practice-management-dialog.html',
+                    controller: 'PracticeManagementDialogController',
+                    controllerAs: 'vm',
+                    backdrop: 'static',
+                    size: 'lg',
+                    resolve: {
+                        entity: ['Practice', function(Practice) {
+                            return Practice.get({login : $stateParams.login});
+                        }]
+                    }
+                }).result.then(function() {
+                    $state.go('practice-management', null, { reload: true });
+                }, function() {
+                    $state.go('^');
+                });
+            }]
+        })
+        .state('practice-management-detail', {
+            parent: 'practice-management',
+            url: '/practice/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-management-detail.html',
+                    controller: 'PracticeManagementDetailController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+       
+        .state('practice-management.delete', {
+            url: '/{login}/delete',
+            data: {
+                authorities: ['ROLE_ADMIN']
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/practice-management/practice-management-delete-dialog.html',
+                    controller: 'PracticeManagementDeleteController',
+                    controllerAs: 'vm',
+                    size: 'md',
+                    resolve: {
+                        entity: ['Practice', function(Practice) {
+                            return Practice.get({login : $stateParams.login});
+                        }]
+                    }
+                }).result.then(function() {
+                    $state.go('practice-management', null, { reload: true });
+                }, function() {
+                    $state.go('^');
+                });
+            }]
+        })
+         .state('practice-management-general', {
+            parent: 'practice-management',
+            url: '/practiceGeneral/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-management-general.html',
+                    controller: 'PracticeManagementGeneralController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+          .state('practice-management-conditions', {
+            parent: 'practice-management',
+            url: '/practiceConditions/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-management-conditions.html',
+                    controller: 'PracticeManagementConditionsController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+        .state('practice-management-thingswork', {
+            parent: 'practice-management',
+            url: '/practiceThingswork/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-management-thingswork.html',
+                    controller: 'PracticeManagementThingsWorkController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+        .state('practice-consult-menu', {
+            parent: 'practice-management',
+            url: '/practiceConsult/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-consult-menu.html',
+                    //controller: 'PracticeManagementThingsWorkController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+        .state('practice-consult-alpha', {
+            parent: 'practice-management',
+            url: '/practiceConsultAlpha/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-consult-alpha.html',
+                    //controller: 'PracticeManagementThingsWorkController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+         .state('practice-consult-workproduct', {
+            parent: 'practice-management',
+            url: '/practiceConsultWorkproduct/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-consult-workproduct.html',
+                    //controller: 'PracticeManagementThingsWorkController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+          .state('practice-consult-ActivitySpace', {
+            parent: 'practice-management',
+            url: '/practiceConsultSpaceActiviy/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-consult-ActivitySpace.html',
+                    //controller: 'PracticeManagementThingsWorkController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+         .state('practice-consult-activity', {
+            parent: 'practice-management',
+            url: '/practiceConsultActiviy/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-consult-activity.html',
+                    //controller: 'PracticeManagementThingsWorkController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+           .state('practice-consult-competency', {
+            parent: 'practice-management',
+            url: '/practiceConsultCompetency/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-consult-competency.html',
+                    //controller: 'PracticeManagementThingsWorkController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+        .state('practice-consult-areaOfConcern', {
+            parent: 'practice-management',
+            url: '/practiceConsultAreaOfConcern/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-consult-areaOfConcern.html',
+                    //controller: 'PracticeManagementThingsWorkController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        })
+        .state('practice-management-spaceActivity', {
+            parent: 'practice-management',
+            url: '/spaceActivity/{login}',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practice-management.detail.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/practice-management-spaceActivity.html',
+                    controller: 'PracticeManagementSpaceActivityController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
