ccd649aab12db47bfaab7102614dd1b8808b5974
Unknown <are.rodriguez.torres@gmail.com>
2017-08-03 13:45:59 -0500

    Kernel-activity Kernel-Competency Kernel-AreaofConcern

--- src/main/webapp/app/practice-management/consult-kernel/consult-kernel.state.js
+++ src/main/webapp/app/practice-management/consult-kernel/consult-kernel.state.js
@@ -51,6 +51,71 @@
                     return $translate.refresh();
                 }]
             }
+        })
+        .state('consult-kernel-competency', {
+            parent: 'app',
+            url: '/consultKernelCompetency/(login)',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practiceManagement.home.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/consult-kernel/consult-kernel-competency.html',
+                    //controller: 'PracticeManagementController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+
+            }        
+        })
+         .state('consult-kernel-activitySpace', {
+            parent: 'app',
+            url: '/consultKernelActivitySpace/(login)',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practiceManagement.home.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/consult-kernel/consult-kernel-activitySpace.html',
+                    //controller: 'PracticeManagementController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+                }        
+        })
+           .state('consult-kernel-areaOfConcern', {
+            parent: 'app',
+            url: '/consultKernelAreaOfConcern/(login)',
+            data: {
+                authorities: ['ROLE_ADMIN'],
+                pageTitle: 'practiceManagement.home.title'
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/practice-management/consult-kernel/consult-kernel-areaOfConcern.html',
+                    //controller: 'PracticeManagementController',
+                    //controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('practice-management');
+                    return $translate.refresh();
+                }]
+
+            }        
         });
     }
 })();
