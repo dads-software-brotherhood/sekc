557cbb9608f4ccdb33bbddc3f9fc195d1588389a
clara.fragoso <clara.fragoso@infotec.mx>
2017-08-09 16:44:28 -0500

    consulta alphas kernel

--- src/main/webapp/app/kernel-management/alphas-management/consult-kernel-alphas.controller.js
+++ src/main/webapp/app/kernel-management/alphas-management/consult-kernel-alphas.controller.js
@@ -14,13 +14,14 @@
         vm.load = load;
         vm.catalogs = catalogs;
         vm.fillAlpha = fillAlpha;
+        vm.fillState =fillState;
         
         vm.load();
         
         
         function load () {
-        	if(angular.isUndefined(localStorageService.get('activitySpaces')) || 
-        			localStorageService.get('activitySpaces') == null){
+        	if(angular.isUndefined(localStorageService.get('alphas')) || 
+        			localStorageService.get('alphas') == null){
             	PracticeCatalogs.query(onSuccess, onError);
         	}else{
         		vm.catalogs();
@@ -29,10 +30,7 @@
 
         function onSuccess(data, headers) {
         	
-        	localStorageService.set('activitySpaces', data.catalogs.activitySpaces);
         	localStorageService.set('alphas', data.catalogs.alphas);
-        	localStorageService.set('workproducts', data.catalogs.workproducts);
-        	localStorageService.set('competencies', data.catalogs.competencies);
         	
         	vm.catalogs();
         	
@@ -43,14 +41,15 @@
         }
         
         function catalogs() {
-        	vm.activitySpaces = localStorageService.get('activitySpaces');
         	vm.alphas = localStorageService.get('alphas');
-        	vm.workproducts = localStorageService.get('workproducts');
-        	vm.competencies = localStorageService.get('competencies');
         }
         
         function fillAlpha(alpha){
         	vm.alphaActual = alpha;
+        }
+        
+        function fillState(state){
+        	vm.stateActual = state;
         }
     }
 })();
