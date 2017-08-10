17c4e56103c647bc0cada0ae905d57a732c7f7e1
clara.fragoso <clara.fragoso@infotec.mx>
2017-08-10 15:39:50 -0500

    cambio de posición find practice

--- src/main/webapp/app/practice-management/practice-consult/practice-consult-general.controller.js
+++ src/main/webapp/app/practice-management/practice-consult/practice-consult-general.controller.js
@@ -5,20 +5,25 @@
         .module('sekcApp')
         .controller('PracticeConsultGeneralController', PracticeConsultGeneralController);
 
-    PracticeConsultGeneralController.$inject = ['$stateParams', 'FindPractice'];
+    PracticeConsultGeneralController.$inject = ['$stateParams', 'FindPractice', '$location'];
 
-    function PracticeConsultGeneralController ($stateParams, FindPractice) {
+    function PracticeConsultGeneralController ($stateParams, FindPractice, $location) {
         var vm = this;
 
         vm.load = load;
         vm.practice = {};
-
-        vm.load($stateParams.idPractice);
+        vm.id = $stateParams.idPractice;
         
-        function load (id) {
-        	FindPractice.get({id: id}, function(result) {
-                vm.practice = result;
-            });
+        vm.load();
+        
+        function load () {
+        	if(vm.id != undefined && vm.id != null && vm.id != ""){
+	        	FindPractice.get({id: vm.id}, function(result) {
+	                vm.practice = result;
+	            });
+        	}else{
+        		$location.path('/find-practice');
+        	}
         }
     }
 })();
