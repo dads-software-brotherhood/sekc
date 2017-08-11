887885002e3fe2ba8df8e391a9b8b01674168f25
clara.fragoso <clara.fragoso@infotec.mx>
2017-08-11 13:29:04 -0500

    sign in top bar

--- src/main/webapp/app/layouts/topnav/topnav.controller.js
+++ src/main/webapp/app/layouts/topnav/topnav.controller.js
@@ -5,9 +5,39 @@
         .module('sekcApp')
         .controller('TopnavController', TopnavController);
 
-    TopnavController.$inject = ['$state', 'Auth', 'Principal', 'ProfileService', 'LoginService'];
+    TopnavController.$inject = ['$scope', '$state', 'Auth', 'Principal', 'ProfileService', 'LoginService'];
 
-    function TopnavController ($state, Auth, Principal, ProfileService, LoginService) {
+    function TopnavController ($scope, $state, Auth, Principal, ProfileService, LoginService) {
+    	var vm = this;
 
+        vm.isAuthenticated = Principal.isAuthenticated;
+
+        $scope.$on('authenticationSuccess', function() {
+            getAccount();
+        });
+
+        getAccount();
+        
+        
+
+        vm.login = login;
+        vm.logout = logout;
+        vm.$state = $state;
+
+        function login() {
+            LoginService.open();
+        }
+
+        function logout() {
+            Auth.logout();
+            $state.go('home');
+            getAccount();
+        }
+        
+        function getAccount() {
+            Principal.identity().then(function(account) {
+                vm.account = account;
+            });
+        }
     }
 })();
